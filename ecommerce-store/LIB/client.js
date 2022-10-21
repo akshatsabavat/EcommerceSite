5dee71b6efa46828b311298d3dd4e3885095b729
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-21 17:27:17 +0530

    hooked up with sanity

--- ecommerce-store/LIB/client.js
+++ ecommerce-store/LIB/client.js
@@ -1,15 +1,13 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-const client = sanityClient({
+export const client = sanityClient({
   projectId: "771umf9o", //Specifies which sanity project to connect to
   token: process.env.SANITY_PROJECT_DATABASE_TKN_KEY, //specific token generated to access sanity api
-  apiVersion: "19-10-2022", //date of when api was made
+  apiVersion: "2022-10-21", //date of when api was made
   dataset: "production", //weather project in development or production
   useCdn: true,
 });
 
-const builder = imageUrlBuilder(sanityClient);
-const urlFor = (source) => builder.image(source);
-
-export { client, builder, urlFor };
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
--- ecommerce-store/pages/index.js
+++ ecommerce-store/pages/index.js
@@ -3,10 +3,15 @@ import React from "react";
 // import HeroBanner from "../components/HeroBanner/HeroBanner";
 // import Product from "../components/Product/Product";
 import { Footer, Product, HeroBanner } from "../components";
-const Home = () => {
+import { client } from "../lib/client";
+const Home = ({ bannerData, products }) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner
+        heroBanner={bannerData.length && bannerData[0]}
+        product={products.length && products[0]}
+      />
+      {console.log(products, bannerData)}
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Check out speakers of many variations</p>
@@ -19,6 +24,18 @@ const Home = () => {
       <Footer />
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
