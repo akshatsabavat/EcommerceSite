f5b194c2bdc528d2f05d76f957ac8978bef8b5b0
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-21 13:35:34 +0530

    corrected file allocation and function call

---
+++ ecommerce-store/components/index.js
@@ -0,0 +1,5 @@
+export { default as HeroBanner } from "./HeroBanner/HeroBanner";
+export { default as Product } from "./Product/Product";
+export { default as Footer } from "./Footer/Footer";
+export { default as Layout } from "./Layout/Layout";
+export { default as Navbar } from "./Navbar/Navbar";
--- ecommerce-store/pages/index.js
+++ ecommerce-store/pages/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+// import Footer from "../components/Footer/Footer";
+// import HeroBanner from "../components/HeroBanner/HeroBanner";
+// import Product from "../components/Product/Product";
+import { Footer, Product, HeroBanner } from "../components";
 const index = () => {
-  return <div>index Page</div>;
+  return (
+    <>
+      <HeroBanner />
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Check out speakers of many variations</p>
+      </div>
+
+      <div>
+        <Product />
+      </div>
+
+      <Footer />
+    </>
+  );
 };
 
 export default index;
