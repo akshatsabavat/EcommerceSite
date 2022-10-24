895af4222426941d4c1a676e61ed85fa0644b99a
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-24 11:19:03 +0530

    retrying stripe payment

--- ecommerce-store/components/Cart/Cart.js
+++ ecommerce-store/components/Cart/Cart.js
@@ -11,26 +11,10 @@ import {
 import { TiDeleteOutline } from "react-icons/ti";
 import { useStateContext } from "../../context/StateContext";
 import { urlFor } from "../../lib/client";
-import getStripe from "../../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
-  const handleCheckout = async () => {
-    const stripe = await getStripe();
-    const response = await fetch("/api/stripe", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(cartItems),
-    });
 
-    if (response.statusCode === 500) return;
-
-    const data = await response.json();
-    toast.loading("Redirecting");
-    stripe.redirectToCheckout({ sessionId: data.id });
-  };
   const {
     toggleCartItemQty,
     totalPrice,
@@ -118,11 +102,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button
-                type="button"
-                className="btn"
-                onClick={() => handleCheckout()}
-              >
+              <button type="button" className="btn" onClick="">
                 Pay with Stripe
               </button>
             </div>
