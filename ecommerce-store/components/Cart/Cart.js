17acf3f9ff8077404262fbb2e739f8ed98641941
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-10-24 12:22:44 +0530

    payment gateway compelete

--- ecommerce-store/components/Cart/Cart.js
+++ ecommerce-store/components/Cart/Cart.js
@@ -11,10 +11,10 @@ import {
 import { TiDeleteOutline } from "react-icons/ti";
 import { useStateContext } from "../../context/StateContext";
 import { urlFor } from "../../lib/client";
+import getStripe from "../../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
-
   const {
     toggleCartItemQty,
     totalPrice,
@@ -23,6 +23,21 @@ const Cart = () => {
     closeCart,
     onRemove,
   } = useStateContext();
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+    const data = await response.json();
+    toast.loading("Redirecting...");
+    stripe.redirectToCheckout({ sessionId: data.id });
+  };
 
   return (
     <div className="cart-wrapper">
@@ -102,8 +117,12 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
-                Pay with Stripe
+              <button
+                type="button"
+                className="btn"
+                onClick={() => handleCheckout()}
+              >
+                Checkout
               </button>
             </div>
           </div>
--- ecommerce-store/pages/api/stripe.js
+++ ecommerce-store/pages/api/stripe.js
@@ -2,6 +2,7 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req.body);
     try {
       const params = {
         submit_type: "pay",
@@ -9,14 +10,31 @@ export default async function handler(req, res) {
         payment_method_types: ["card"],
         billing_address_collection: "auto",
         shipping_options: [{ shipping_rate: "shr_1Lw7NDSBtSNzCDMOAW5KSjQl" }],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/771umf9o/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
